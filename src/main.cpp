c8855816de367256e26c164291715d3f8ef50989
ansh-codr <anshloverao@gmail.com>
2026-02-21 22:03:34 +0530

    add Servo library and declare servo object

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 /**
  * @file main.cpp
  * @brief Simple servo sweep demonstration
- * @author ansh-codr (Ansh Yadav)
+ * @author ansh-codr
  * @date 2026-02-21
  */
+
+Servo myServo;
 
 void setup() {
 }
